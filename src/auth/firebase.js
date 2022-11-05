a7a0b972f93728a77e379d0db1df053cb24a9c68
Mehmet Arslan <marslan.developer@gmail.com>
2022-11-05 16:28:32 +0300

    firebase added

---
+++ src/auth/firebase.js
@@ -0,0 +1,85 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+  sendPasswordResetEmail,
+} from "firebase/auth";
+
+// TODO: Replace the following with your app's Firebase project configuration at project settings part
+// See: https://firebase.google.com/docs/web/learn-more#config-object
+const firebaseConfig = {
+  apiKey: "AIzaSyBXauxj0CUNKQEpdYdOTRzshs95hxqgcig",
+  authDomain: "movie-app-project-313ec.firebaseapp.com",
+  projectId: "movie-app-project-313ec",
+  storageBucket: "movie-app-project-313ec.appspot.com",
+  messagingSenderId: "892898684679",
+  appId: "1:892898684679:web:17bfe77b878df7725f5fa2",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);
+
+export const createUser = (email, password) => {
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+export const signInUser = (email, password) => {
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      console.log(user);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+const provider = new GoogleAuthProvider();
+
+export const signInPopUp = () => {
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      // The signed-in user info.
+      const user = result.user;
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      console.log(error);
+    });
+};
+
+export const signOutUser = () => {
+  signOut(auth)
+    .then(() => {
+      // Sign-out successful.
+    })
+    .catch((error) => {
+      // An error happened.
+    });
+};
+
+export const resetMail = (email) => {
+  sendPasswordResetEmail(auth, email)
+    .then(() => {
+      // Password reset email sent!
+    })
+    .catch((error) => {
+      const errorMessage = error.message;
+      // ..
+    });
+};
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,47 +1,22 @@
-import { useState } from "react";
-import Container from "react-bootstrap/Container";
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
+import React, { useState } from "react";
+import { signInUser } from "../auth/firebase";
 
 const Login = () => {
-  const [user, setUser] = useState({ email: "", password: "" });
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
-
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
-    <Container>
-      <h1 className="text-center mt-4">LOGIN PAGE</h1>
-      <Form onSubmit={(e) => handleSubmit(e)}>
-        <Form.Group className="mb-3" controlId="username">
-          <Form.Label>Email</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Enter your email"
-            name="email"
-            value={user?.email}
-            onChange={(e) => setUser({ ...user, email: e.target.value })}
-          />
-        </Form.Group>
-
-        <Form.Group className="mb-3" controlId="password">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Enter your password"
-            name="password"
-            value={user?.password}
-            onChange={(e) => setUser({ ...user, password: e.target.value })}
-          />
-        </Form.Group>
-        <Container className="text-center">
-          <Button variant="danger" type="submit">
-            Submit
-          </Button>
-        </Container>
-      </Form>
-    </Container>
+    <div>
+      <input type="email" onChange={(e) => setEmail(e.target.value)}></input>
+      <br />
+      <input
+        type="password"
+        onChange={(e) => setPassword(e.target.value)}
+      ></input>
+      <br />
+      <button onClick={() => signInUser({ email, password })}>sign in</button>
+      <br />
+      <button>sign in with google</button>
+    </div>
   );
 };
 
